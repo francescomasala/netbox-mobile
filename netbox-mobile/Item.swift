5741b0edabf94625a7875e0ba759a1ef8dee07a8
Francesco Masala <fmasala@hyperbit.it>
2026-06-13 17:26:21 +0200

    Initial Commit

---
+++ netbox-mobile/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  netbox-mobile
+//
+//  Created by Francesco Masala on 13/06/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ netbox-mobileTests/netbox_mobileTests.swift
@@ -0,0 +1,18 @@
+//
+//  netbox_mobileTests.swift
+//  netbox-mobileTests
+//
+//  Created by Francesco Masala on 13/06/26.
+//
+
+import Testing
+
+struct netbox_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
